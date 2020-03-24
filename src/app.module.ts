53a37abdee1b85b5a59136d98d40f51ef03bf7fd
pepesan <pepesan@gmail.com>
2020-03-24 18:28:59 +0100

    ejemplos simples y no tan simples de rest pyload rutas etc...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RutasController } from './rutas/rutas.controller';
+import { PatronesController } from './patrones/patrones.controller';
+import { ParamsController } from './params/params.controller';
+import { RestController } from './rest/rest.controller';
+import { PayloadController } from './payload/payload.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, RutasController, PatronesController, ParamsController, RestController, PayloadController],
   providers: [AppService],
 })
 export class AppModule {}
