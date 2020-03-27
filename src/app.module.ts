9f57b0ad36636f5f69497386dcb8554312cec4c4
pepesan <pepesan@gmail.com>
2020-03-27 19:06:23 +0100

    quitado funcionalidad obsoleta de enganche con mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import {MongooseModule} from '@nestjs/mongoose';
   imports: [
     ApiModule,
     MongooseModule.forRoot('mongodb://localhost:27017/test',
-    { useNewUrlParser: true, useUnifiedTopology: true })],
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })],
   controllers: [AppController,
     RutasController,
     PatronesController,
